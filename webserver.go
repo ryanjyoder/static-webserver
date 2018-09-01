52fcfd3757e50bdf8f03668823ea9fdaa1a4bd45
Ryan J Yoder <ryanathome@Ryans-MacBook-Pro.local>
2018-09-01 13:42:27 -0700

    removed println

--- webserver.go
+++ webserver.go
@@ -12,6 +12,5 @@ func main() {
 	port := flag.Int("port", 8080, "Port to listen on (default: 8080)")
 	dir := flag.String("dir", ".", "Directory to server from (default: .)")
 	flag.Parse()
-	fmt.Println(*port, *dir)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), http.FileServer(http.Dir(*dir))))
 }
